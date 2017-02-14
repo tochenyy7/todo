31c4e87ad2fc81970e2b30bedd4c893796ba2e8d
tochenyy7 <tochenyy7@gmail.com>
2017-02-14 20:06:40 +0200

    todo

---
+++ list.js
@@ -0,0 +1,34 @@
+﻿function ListCtrl($scope) {
+
+    $scope.items = [
+      { text: 'Декадное совещание', date: '30-01-2017', done: true },
+      { text: 'Балансовое совещание', date: '12-02-2017', done: false },
+      { text: 'Встреча с заказчиком', date: '01-02-2017', done: true },
+      { text: 'Банкет', date: '10-02-2017', done: false },
+    ];
+ 
+    $scope.addItem = function () {
+		$scope.items.push({ text: $scope.todo, date: $scope.setDate, done: false });
+        $scope.todo = '';
+    };
+ 
+
+    $scope.remain = function () {
+        var count = 0;
+        angular.forEach($scope.items, function(item) {
+            count += item.done;
+        });
+        return count;
+    };
+
+   $scope.delete = function(item) { 
+    var index = $scope.items.indexOf(item); 
+    $scope.items.splice(index, 1); 
+   };
+}
+
+
+       
+
+
+
